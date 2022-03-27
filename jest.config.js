96a6df1ab9d6e11f0342098eb87355990787fc6a
somq14 <som.q14@gmail.com>
2022-03-27 23:16:50 +0900

    🔧 configure jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // TypeScript は ts-jest でトランスパイルする
+  transform: { "^.+\\.ts$": "ts-jest" },
+  // src からテストファイルを検索
+  roots: ["<rootDir>/src"],
+  // テストがないファイルも含めてカバレッジを取得する
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+};
