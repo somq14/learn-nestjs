89090a8b6579e9e2f32dc423209df2fad8a33ed1
somq14 <som.q14@gmail.com>
2022-04-02 00:39:49 +0900

    ✨ Docker container for heroku

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
 
+const port = Number(process.env["PORT"]) ?? 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 void bootstrap();
