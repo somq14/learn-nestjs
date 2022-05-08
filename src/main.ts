7bc20c049e428a66b1ae3dd9e367b1c8346d6cfd
somq14 <som.q14@gmail.com>
2022-05-08 19:31:32 +0900

    🔧 Configure yarn start

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
 
-const port = Number(process.env["PORT"]) ?? 3000;
+const port = Number(process.env["PORT"] ?? "3000");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
